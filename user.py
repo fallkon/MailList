5c0a3694e4e959d143fab30b943b28a39dcbd43b
Ivan Kotov <ivan_kotov@abv.bg>
2014-03-20 20:13:28 +0200

    mail lists

---
+++ user.py
@@ -0,0 +1,11 @@
+class User():
+	"""docstring for ClassName"""
+	def __init__(self, name, mail):
+		self.name = name
+		self.mail = mail
+
+	def take_name(self):
+		return self.name
+
+	def take_mail(self):
+		return self.mail
