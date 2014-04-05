807eb49da2f1ef86c6425c08f4d9d0d7e87c6d7f
oLeG Kamenshchik <o.trulla@gmail.com>
2014-04-05 19:06:29 +0300

    Update maillist.py

--- maillist.py
+++ maillist.py
@@ -1,36 +1,33 @@
 from user import User
 
+
 class MailList():
 
-	def __init__(self, name,id_list):
-		self.name = name
-		self.id = id_list
-		self.users = []
+    def __init__(self, list_id, name):
+        self.list_id = list_id
+        self.name = name
+        self.users = []
 
-	def getName(self):
-		return self.name
+    def get_name(self):
+        return self.name
 
-	def getId(self):
-		return self.id
+    def add_user(self, userName, userEmail):
+        newUser = User(userName, userEmail)
+        self.users.append(newUser)
 
+    def print_user(self):
+        res = []
+        indx = 1
+        for item in self.users:
+            res.append('[{0}] {1} - {2}'.format(indx, item.name, item.email))
+            indx += 1
+        return '\n'.join(res)
 
-	def addUser(self, userName, userEmail):
-		newUser = User(userName, userEmail)
-		self.users.append(newUser)
-
-	def printUser(self):
-		result = []
-		index = 1
-		for item in self.users:
-			result.append('[{0}]  {1} - {2}'.format(index, item.name, item.email))
-			index += 1
-		return '\n'.join(result)
-
-	def searchEmail(self, email):
-		for item in self.users:
-			if email == item.email:
-				return True
-			return False
+    def search_email(self, email):
+        for item in self.users:
+            if email == item.email:
+                return True
+        return False
 
     def search_name(self, name):
         for item in self.users:
